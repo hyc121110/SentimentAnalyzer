4da21e3f50884ab257742d903d1ae83ca1b0c8be
hchan069 <spagabtti@gmail.com>
2019-05-19 14:35:19 -0700

    changed var names

--- test.py
+++ test.py
@@ -9,16 +9,17 @@ model4 = pickle.load(open("models/model_rf.pk1", "rb"))
 count_vec = pickle.load(open("models/count_vec.pk1", "rb"))
 
 # Try with user input
-print("Please type a review: ", end='')
+print("Please enter a review of a company: ", end='')
 sentence = str(input())
 sentence = [word for word in sentence.split() if word.lower() not in stop_words]
 sentence = " ".join(sentence)
-sentence = count_vec.transform([sentence])
+sent = count_vec.transform([sentence])
 
 # pick one model (lr has the highest acc)
-pred1 = model1.predict(sentence)[0]
-pred2 = model2.predict(sentence)[0]
-pred3 = model3.predict(sentence)[0]
-pred4 = model4.predict(sentence)[0]
-res = "This is a positive review!" if pred3 == 1 else "This is a negative review!"
+pred1 = model1.predict(sent)[0]
+pred2 = model2.predict(sent)[0]
+pred3 = model3.predict(sent)[0]
+pred4 = model4.predict(sent)[0]
+pred = pred1 + pred2 + pred3 + pred4
+res = "\nThis is a positive review!" if pred > 2 else "This is a negative review!"
 print(res)
