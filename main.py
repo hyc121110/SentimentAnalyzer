3fd8413c07bd1934c0d676f873a043b1a9046003
hchan069 <spagabtti@gmail.com>
2019-05-19 21:41:11 -0700

    added plots

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 # Import libraries
 
+# sklearn
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import LinearSVC
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+import numpy as np
 
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import pickle
 import os
 import re
 import time
+from collections import Counter
+from collections import defaultdict
+
+# Word Cloud 
+from wordcloud import WordCloud
+
+# plotly
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from plotly import tools
+init_notebook_mode(connected=True)
 
 # nltk
 from nltk.corpus import stopwords
@@ -20,6 +38,7 @@ stop_words=set(stopwords.words('english'))
 from nltk.tokenize import RegexpTokenizer
 tokenizer = RegexpTokenizer(r'\w+')
 from nltk.stem import WordNetLemmatizer
+from nltk import ngrams, word_tokenize
 
 # initialize dataframe
 df = pd.read_csv('data/employee_reviews.csv', index_col=0)
@@ -113,18 +132,196 @@ pickle.dump(nb, open(path + "/" + "model_lr.pk1", "wb"))
 print("Model Log Reg created")
 
 # Random Forest: ~90% accuracy with the following params values (3 mins)
-start = time.time()
-rf = RandomForestClassifier(max_depth=100, n_estimators=100).fit(X_train, y_train)
-y_pred = rf.predict(X_test)
+#start = time.time()
+#rf = RandomForestClassifier(max_depth=100, n_estimators=300).fit(X_train, y_train)
+#y_pred = rf.predict(X_test)
+#
+#acc = accuracy_score(y_test, y_pred)
+#print("Random Forest Accuracy on the companies dataset: {:.2f}%".format(acc*100))
+#pickle.dump(nb, open(path + "/" + "model_rf.pk1", "wb"))
+#print("Model Random Forest created")
+#
+#end = time.time()
+#print("Time elapsed: {} seconds".format(end - start))
 
-acc = accuracy_score(y_test, y_pred)
-print("Random Forest Accuracy on the companies dataset: {:.2f}%".format(acc*100))
-pickle.dump(nb, open(path + "/" + "model_rf.pk1", "wb"))
-print("Model Random Forest created")
+# Graph stuff
 
-end = time.time()
-print("Time elapsed: {} seconds".format(end - start))
+# Visualising the results using Confusion Matrix
+print("Confustion Matrix")
+mat = confusion_matrix(y_test, y_pred)
+sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False,
+            xticklabels=['pros','cons'], yticklabels=['pros','cons'])
+plt.xlabel('true label')
+plt.ylabel('predicted label')
+plt.show()
+
+# Pros Word Cloud
+pros_cloud = list()
+for sent in pros_lem:
+    tmp = sent.split()
+    pros_cloud += tmp
+
+print("Word Cloud of Pros Review")
+word_cnt = Counter(pros_cloud)
+reviews_cloud = WordCloud(background_color='black', width=1280, height=720).generate_from_frequencies(word_cnt)
+plt.imshow(reviews_cloud)
+plt.axis('off')
+plt.show()
+
+# Cons Word Cloud
+cons_cloud = list()
+for sent in cons_lem:
+    tmp = sent.split()
+    cons_cloud += tmp
+
+print("Word Cloud of Cons Review")
+word_cnt = Counter(cons_cloud)
+reviews_cloud = WordCloud(background_color='black', width=1280, height=720).generate_from_frequencies(word_cnt)
+plt.imshow(reviews_cloud)
+plt.axis('off')
+plt.show()
 
 
+# n grams
 
+plot_path = 'plot'
+
+## custom function for horizontal bar chart ##
+def horizontal_bar_chart(df, color):
+    trace = go.Bar(
+        y=df["word"].values[::-1],
+        x=df["wordFreq"].values[::-1],
+        showlegend=False,
+        orientation = 'h',
+        marker=dict(
+            color=color,
+        )
+    )
+    return trace
+
+# tokenize words in pros/cons_lemm for n grams
+pros_token = list()
+cons_token = list()
+for p, c in zip(pros_lem, cons_lem):
+    pros_token.append(word_tokenize(p))
+    cons_token.append(word_tokenize(c))
+    
+reviews_batch = pros_token + cons_token
+
+
+# Plotting the bigram plot (Pros)
+reviews_df = pd.DataFrame({'Reviews':pros_token})
+bigram_dict = defaultdict(int)
+
+for sentence in reviews_df["Reviews"]:
+    if len(sentence) > 1:
+        bigram = ngrams(sentence, 2)
+        for gram in bigram:
+            bigram_dict[" ".join([g for g in gram])] += 1
+
+
+fd_sorted = pd.DataFrame(sorted(bigram_dict.items(), key=lambda x: x[1])[::-1])
+fd_sorted.columns = ["word", "wordFreq"]
+sym_2 = horizontal_bar_chart(fd_sorted.head(50), '#CC3333')
+                             
+fig = tools.make_subplots(
+        rows=1, 
+        cols=2, 
+        vertical_spacing=0.04,
+        horizontal_spacing=0.05,
+        subplot_titles=["","Frequent Bigrams of Reviews"])
+fig.append_trace(sym_2, 1, 2)
+fig['layout'].update(
+        height=1080, 
+        width=800, 
+        paper_bgcolor='rgb(233,233,233)', 
+        title="Bigram Plots of Reviews Keywords after removing Stopwords")
+plotly.offline.plot(fig, filename='pros-bigram-word-plots.html')
+#iplot(fig, filename='bigram-word-plots') # jupyter notebook only
+
+# Plotting the trigram plot
+trigram_dict = defaultdict(int)
+
+for sentence in reviews_df["Reviews"]:
+    if len(sentence) > 1:
+        trigram = ngrams(sentence, 3)
+        for gram in trigram:
+            trigram_dict[" ".join([g for g in gram])] += 1
+        
+fd_sorted = pd.DataFrame(sorted(trigram_dict.items(), key=lambda x: x[1])[::-1])
+fd_sorted.columns = ["word", "wordFreq"]
+sym_3 = horizontal_bar_chart(fd_sorted.head(50), '#333399')
+                             
+fig = tools.make_subplots(
+        rows=1, 
+        cols=2, 
+        vertical_spacing=0.04,
+        horizontal_spacing=0.05,
+        subplot_titles=["", "Frequent Trigrams of Reviews"])
+fig.append_trace(sym_3, 1, 2)
+fig['layout'].update(
+        height=1080, 
+        width=800, 
+        paper_bgcolor='rgb(233,233,233)', 
+        title="Trigram Plots of Reviews Keywords after removing Stopwords")
+plotly.offline.plot(fig, filename='pros-trigram-word-plots.html')
+#iplot(fig, filename='trigram-word-plots') # jupyter notebook only
+
+# Plotting the bigram plot (Cons)
+reviews_df = pd.DataFrame({'Reviews':cons_token})
+bigram_dict = defaultdict(int)
+
+for sentence in reviews_df["Reviews"]:
+    if len(sentence) > 1:
+        bigram = ngrams(sentence, 2)
+        for gram in bigram:
+            bigram_dict[" ".join([g for g in gram])] += 1
+
+
+fd_sorted = pd.DataFrame(sorted(bigram_dict.items(), key=lambda x: x[1])[::-1])
+fd_sorted.columns = ["word", "wordFreq"]
+sym_2 = horizontal_bar_chart(fd_sorted.head(50), '#CC3333')
+                             
+fig = tools.make_subplots(
+        rows=1, 
+        cols=2, 
+        vertical_spacing=0.04,
+        horizontal_spacing=0.05,
+        subplot_titles=["","Frequent Bigrams of Reviews"])
+fig.append_trace(sym_2, 1, 2)
+fig['layout'].update(
+        height=1080, 
+        width=800, 
+        paper_bgcolor='rgb(233,233,233)', 
+        title="Bigram Plots of Reviews Keywords after removing Stopwords")
+plotly.offline.plot(fig, filename='cons-bigram-word-plots.html')
+#iplot(fig, filename='bigram-word-plots') # jupyter notebook only
+
+# Plotting the trigram plot
+trigram_dict = defaultdict(int)
+
+for sentence in reviews_df["Reviews"]:
+    if len(sentence) > 1:
+        trigram = ngrams(sentence, 3)
+        for gram in trigram:
+            trigram_dict[" ".join([g for g in gram])] += 1
+        
+fd_sorted = pd.DataFrame(sorted(trigram_dict.items(), key=lambda x: x[1])[::-1])
+fd_sorted.columns = ["word", "wordFreq"]
+sym_3 = horizontal_bar_chart(fd_sorted.head(50), '#333399')
+                             
+fig = tools.make_subplots(
+        rows=1, 
+        cols=2, 
+        vertical_spacing=0.04,
+        horizontal_spacing=0.05,
+        subplot_titles=["", "Frequent Trigrams of Reviews"])
+fig.append_trace(sym_3, 1, 2)
+fig['layout'].update(
+        height=1080, 
+        width=800, 
+        paper_bgcolor='rgb(233,233,233)', 
+        title="Trigram Plots of Reviews Keywords after removing Stopwords")
+plotly.offline.plot(fig, filename='cons-trigram-word-plots.html')
+#iplot(fig, filename='trigram-word-plots') # jupyter notebook only
 
